6f06e2e62251068730de418b63a4d147e32c3ce2
guoxy <guoxy@hxmec.com>
2022-11-28 19:25:19 +0800

    fix:test

--- compile/build.sh
+++ compile/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-echo "start compile hsa-spp-static"
+echo "start compile"
 
-rm -rf static
-rm -f static.zip
-mkdir static
-mkdir static/www
-cp -r dist/* static/www/
-cp tencent-five/* static/
-zip -r static.zip static/
+ls
+pwd
+cp -r dist/* www/html/
+# zip -r dist.zip dist/
